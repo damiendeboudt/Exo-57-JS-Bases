c1632e683a096529c0f59289f79a94b9c630fce1
Damien <damien.deboudt@sfr.fr>
2022-11-19 16:36:43 +0100

    ok bro

---
+++ exo 5/script.js
@@ -0,0 +1,6 @@
+let age = prompt("mettre un nombre à virgule");
+let age2 = prompt("mettre un deuxième nombre à virgule");
+
+let age3 = parseInt(age)
+let result = (age3 * age2);
+alert(result);
---
+++ exo 6/script2.js
@@ -0,0 +1,9 @@
+let age = prompt("entrez votre âge")
+
+let result = age;
+
+if ( result < 18) {
+    alert("vous êtes mineur")
+} else {
+    alert("vous êtes majeur")
+}
